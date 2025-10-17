ac66a925e5d7c242afafc2ecd8259f4eadca99b8
GF63 <152105287+Engi-is-engihere@users.noreply.github.com>
2025-10-17 23:24:48 +0300

    Обнова

---
+++ ShopApp/ProductForm.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace ShopApp
+{
+    partial class ProductForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.ProductPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(39, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(280, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(352, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(270, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(652, 28);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(270, 24);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(951, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Корзина";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(39, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 46);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Обновить";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ProductPanel
+            // 
+            this.ProductPanel.AutoScroll = true;
+            this.ProductPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.ProductPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ProductPanel.Location = new System.Drawing.Point(0, 118);
+            this.ProductPanel.Name = "ProductPanel";
+            this.ProductPanel.Size = new System.Drawing.Size(1382, 615);
+            this.ProductPanel.TabIndex = 5;
+            // 
+            // ProductForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1382, 733);
+            this.Controls.Add(this.ProductPanel);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.MaximumSize = new System.Drawing.Size(1400, 780);
+            this.MinimumSize = new System.Drawing.Size(1400, 780);
+            this.Name = "ProductForm";
+            this.Text = "ProductForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FlowLayoutPanel ProductPanel;
+    }
+}
---
+++ ShopApp/ProductForm.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShopApp
+{
+    public partial class ProductForm : Form
+    {
+        public ProductForm()
+        {
+            InitializeComponent();
+            UpdateProduct();    
+        }
+        public void UpdateProduct() {
+            GroupBox groupBox = new GroupBox();
+            groupBox.Width = 200; 
+            groupBox.Height = 300; 
+            groupBox.Text = ""; 
+
+            
+            PictureBox pictureBox = new PictureBox();
+            pictureBox.Image = Properties.Resources.Computer_nerd_1536x1054;
+            pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox.Width = 180;
+            pictureBox.Height = 120;
+            pictureBox.Top = 10;
+            pictureBox.Left = 10;
+
+            Label labelName = new Label();
+            labelName.Text = "Название товара";
+            labelName.Top = pictureBox.Bottom + 10;
+            labelName.Left = 10;
+            labelName.AutoSize = true;
+
+            Label labelPrice = new Label();
+            labelPrice.Text = "Цена: 1000 ₽";
+            labelPrice.Top = labelName.Bottom + 5;
+            labelPrice.Left = 10;
+            labelPrice.AutoSize = true;
+
+            Button btnAddToCart = new Button();
+            btnAddToCart.Text = "В корзину";
+            btnAddToCart.Width = 80;
+            btnAddToCart.Top = labelPrice.Bottom + 10;
+            btnAddToCart.Left = 10;
+
+            Button btnReview = new Button();
+            btnReview.Text = "Отзыв";
+            btnReview.Width = 80;
+            btnReview.Top = labelPrice.Bottom + 10;
+            btnReview.Left = btnAddToCart.Right + 10;
+
+            Button btnOrder = new Button();
+            btnOrder.Text = "Оформить заказ";
+            btnOrder.Width = 80;
+            btnOrder.Top = labelPrice.Bottom + 10;
+            btnOrder.Left = btnReview.Right + 10;
+
+            groupBox.Controls.Add(pictureBox);
+            groupBox.Controls.Add(labelName);
+            groupBox.Controls.Add(labelPrice);
+            groupBox.Controls.Add(btnAddToCart);
+            groupBox.Controls.Add(btnReview);
+            groupBox.Controls.Add(btnOrder);
+
+            ProductPanel.Controls.Add(groupBox);
+        
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
